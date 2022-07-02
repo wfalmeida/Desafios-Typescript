f7db556275b50b780bed8dac577742d59574d453
William Felipe de Almeida <williamfelipeab@gmail.com>
2022-07-01 21:24:22 -0300

    Desafio 1 finalizado

---
+++ Desafio 1/README.md
@@ -0,0 +1,53 @@
+# Desafio 1 - Refatoração de código para TypeScript
+
+ 
+Enunciado: refatorar o código JS abaixo para que não apresente erros utilizando Typescript.
+
+``` javascript
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+```
+
+Feateres do desafio executado:
+
+* Elaboração de uma interface para criação de um "empregado".
+``` javascript
+interface IEmployee {
+    code: number,
+    name: string
+}
+```
+* Elaboração de um método para criar um novo empregado.
+```javascript
+const createNewEmployee = () : IEmployee => {
+    const newEmployee: IEmployee = {
+        name: inputName.value,
+        code: Number(inputCode.value)
+    }
+
+    return newEmployee;
+}
+```
+* Elaboração de um método para criar um novo item HTML na lista de empregados.
+```javascript
+const addNewEmployeeToList = () => {
+
+    const employee: IEmployee = createNewEmployee();
+
+    const newItem = `<li class="employee-item">
+                        <span>
+                            ${employee.name}
+                        </span>
+                        <span>
+                            ${employee.code}
+                        </span>
+                    </li>`
+    
+    employeesList.innerHTML += newItem;
+}
+```
+* Elaboração de uma interface responsiva para uma melhor visualização do desafio.
+
+![Interface Gráfica para cadastrar empregados - GIF](/assets/images/GIF.gif)
+
---
+++ Desafio 1/src/app.ts
@@ -0,0 +1,48 @@
+const inputCode = document.querySelector('[name = "code"]') as HTMLInputElement;
+const inputName = document.querySelector('[name = "name"]') as HTMLInputElement;
+const submitButton = document.querySelector('button[type="submit"]')
+const employeesList = document.querySelector('ul') as HTMLDListElement;
+const form = document.querySelector('.form') as HTMLFormElement;
+
+let error = "";
+
+interface IEmployee {
+    code: number,
+    name: string
+}
+
+const createNewEmployee = () : IEmployee => {
+    const newEmployee: IEmployee = {
+        name: inputName.value,
+        code: Number(inputCode.value)
+    }
+
+    return newEmployee;
+}
+
+const addNewEmployeeToList = () => {
+
+    const employee: IEmployee = createNewEmployee();
+
+    const newItem = `<li class="employee-item">
+                        <span>
+                            ${employee.name}
+                        </span>
+                        <span>
+                            ${employee.code}
+                        </span>
+                    </li>`
+    
+    employeesList.innerHTML += newItem;
+}
+
+
+const nullInputs = !inputCode && !inputName;
+const codeIsNumber = typeof Number(inputCode.value) === "number" ? true : false;
+
+if(form && !nullInputs && codeIsNumber) {
+    form.addEventListener('submit', (event: SubmitEvent) => {
+        event.preventDefault();
+        addNewEmployeeToList();
+    })
+}
